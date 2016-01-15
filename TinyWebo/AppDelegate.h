f11d50e84a7270f9479cc92647d7fbbeb8e8dce7
fengchao <fengc09@me.com>
2016-01-15 11:38:30 +0800

    Initial Commit

---
+++ TinyWebo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TinyWebo
+//
+//  Created by fengchao on 16/1/15.
+//  Copyright © 2016年 fengchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TinyWeboTests/TinyWeboTests.m
@@ -0,0 +1,39 @@
+//
+//  TinyWeboTests.m
+//  TinyWeboTests
+//
+//  Created by fengchao on 16/1/15.
+//  Copyright © 2016年 fengchao. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TinyWeboTests : XCTestCase
+
+@end
+
+@implementation TinyWeboTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
